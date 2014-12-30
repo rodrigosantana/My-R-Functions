12eed53c3039cf97958b5248ad0a72f8ed5666e0
Rodrigo Sant'Ana <santana@chewie.home>
2014-12-30 21:32:56 -0200

    Function to drawing a Compass Rose in Maps based in ggplot2 - update

--- compass.rose.R
+++ compass.rose.R
@@ -1,5 +1,21 @@
-### This function inserts a Compass Rose in Maps developed with 
-### ggplot2 R package...
+########################################################################
+## Description: Function to insert a Compasse Rose (North Arrow) in Maps
+## developed with ggplot2 R package
+##
+## Maintainer: Rodrigo Sant'Ana
+## Author: Rodrigo Sant'Ana
+## Created: Ter Dez 30 21:27:56 2014 (-0200)
+## Version: 0.0.1
+## Last-Updated: Ter Dez 30 21:29:51 2014 (-0200)
+##           By: Rodrigo Sant'Ana
+##
+## Database info: No data info...
+##
+### Commentary:
+##
+### Code:
+########################################################################
+
 compass.rose <- function(loc, size, bearing = 0, cex = 1, w.subplot,
         	             h.subplot, ...) {
     ### Loading required packages...
@@ -49,3 +65,22 @@ compass.rose <- function(loc, size, bearing = 0, cex = 1, w.subplot,
                 theme_nothing()
     print(wr, vp = vp)
 }
+
+########################################################################
+##
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 3, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; see the file COPYING.  If not, write to
+## the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+## Floor, Boston, MA 02110-1301, USA.
+##
+########################################################################
